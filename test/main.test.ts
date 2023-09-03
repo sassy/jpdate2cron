23d41cf4dafc73b13969847fe72f88cad0401c9e
Satoshi Watanabe <sassy.watanabe@gmail.com>
2023-09-03 11:53:35 +0900

    fix '-' case

--- test/main.test.ts
+++ test/main.test.ts
@@ -1,7 +1,21 @@
-import { convertCron } from "../src/main";
+import { convertCron, convertISODate } from "../src/main";
 
 test("check", () => {
-  const ret = convertCron("2023/01/01");
-  expect(ret).toBe("* * 31 12 *");
+  const ret = convertCron("2023/01/01 12:00");
+  expect(ret).toBe("* * 1 1 *");
 });
-  
+
+test("check convertISODate", () => {
+  const ret1 = convertISODate("2023/01/01");
+  expect(ret1).toBe("2023-01-01");
+
+  const ret2 = convertISODate("2023-01-01");
+  expect(ret2).toBe("2023-01-01");
+}); 
+
+test("check convertISODate for not zero padding)", () => {
+  const ret1 = convertISODate("2023/1/1");
+  expect(ret1).toBe("2023-1-1");
+}); 
+
+
